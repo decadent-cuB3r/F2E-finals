c424ef77cdcb97b22d37688cc1c1f3a68fb613ed
decadent-cuB3r <71422011+decadent-cuB3r@users.noreply.github.com>
2021-01-06 02:19:54 +0800

    frame RWD Hamburger

--- js/frame.js
+++ js/frame.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
     $('.menu-btn').click(function() {
         $(this).toggleClass('open')
     })
+    $('.menu-btn').click(function() {
+        $('.nav_list').toggleClass('open')
+    })
 });
