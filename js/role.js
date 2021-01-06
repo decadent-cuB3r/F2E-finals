602b3f9d622c8234664c1bfbfbcd4834f97d2071
peggy1008123 <peggy1008123@yahoo.com.tw>
2021-01-07 04:32:08 +0800

    scrollintoview

---
+++ js/role.js
@@ -0,0 +1,53 @@
+function myFunction1() {
+    var e = document.getElementById("role1");
+    e.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth',
+        inline: 'start'
+    });
+}
+
+function myFunction2() {
+    var e = document.getElementById("role2");
+    e.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth',
+        inline: 'start'
+    });
+}
+
+function myFunction3() {
+    var e = document.getElementById("role3");
+    e.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth',
+        inline: 'start'
+    });
+}
+
+function myFunction4() {
+    var e = document.getElementById("role4");
+    e.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth',
+        inline: 'start'
+    });
+}
+
+function myFunction5() {
+    var e = document.getElementById("role5");
+    e.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth',
+        inline: 'start'
+    });
+}
+
+function myFunction6() {
+    var e = document.getElementById("role6");
+    e.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth',
+        inline: 'start'
+    });
+}
